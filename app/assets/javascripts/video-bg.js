de1c851eebb9d2561e3e2bbba2b0d8e308cb1f1d
Gino Capio <ginocapio@gmail.com>
2018-03-11 17:54:51 -0500

    Add files needed for video-bg

---
+++ app/assets/javascripts/video-bg.js
@@ -0,0 +1,93 @@
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var tv,
+    playerDefaults = {
+        autoplay: 0,
+        autohide: 1,
+        modestbranding: 0,
+        rel: 0,
+        showinfo: 0,
+        controls: 0,
+        disablekb: 1,
+        enablejsapi: 0,
+        iv_load_policy: 3
+    };
+var vid = [
+        {'videoId': '2hrG4lIyv2I', 'suggestedQuality': 'hd1080p'},
+        {'videoId': 'J0U0TqWUjFQ', 'startSeconds': 82, 'endSeconds': 112, 'suggestedQuality': 'hd720p60'},
+        {'videoId': 'J0U0TqWUjFQ', 'startSeconds': 235, 'endSeconds': 269, 'suggestedQuality': 'hd720p60'},
+        {'videoId': 'J0U0TqWUjFQ', 'startSeconds': 380, 'endSeconds': 425, 'suggestedQuality': 'hd720p60'}
+    ],
+    randomVid = Math.floor(Math.random() * vid.length),
+    currVid = randomVid;
+
+$('.sound em:last-of-type').html(vid.length);
+
+function onYouTubePlayerAPIReady() {
+    tv = new YT.Player('tv', {
+        events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange},
+        playerVars: playerDefaults
+    });
+}
+
+function onPlayerReady() {
+    tv.loadVideoById(vid[currVid]);
+    tv.mute();
+}
+
+function onPlayerStateChange(e) {
+    if (e.data === 1) {
+        $('#tv').addClass('active');
+        $('.sound em:nth-of-type(1)').html(currVid + 1);
+    } else if (e.data === 2) {
+        $('#tv').removeClass('active');
+        if (currVid === vid.length - 1) {
+            currVid = 0;
+        } else {
+            currVid++;
+        }
+        tv.loadVideoById(vid[currVid]);
+        tv.seekTo(vid[currVid].startSeconds);
+    }
+}
+
+function vidRescale() {
+
+    var w = $(window).width() + 200,
+        h = $(window).height() + 200;
+
+    if (w / h > 16 / 9) {
+        tv.setSize(w, w / 16 * 9);
+        $('.tv .screen').css({'left': '0px'});
+    } else {
+        tv.setSize(h / 9 * 16, h);
+        $('.tv .screen').css({'left': -($('.tv .screen').outerWidth() - w) / 2});
+    }
+}
+
+$(window).on('load resize', function () {
+    vidRescale();
+});
+
+$('.sound   .mute-btn').on('click', function () {
+    $('#tv').toggleClass('mute');
+    $('.sound i:first-of-type').toggleClass('hidden');
+    if ($('#tv').hasClass('mute')) {
+        tv.mute();
+        $('.fa-volume-off').hide();
+        $('.fa-volume-up').show();
+        $('.toggle-text').text('TOGGLE SOUND ON');
+    } else {
+        tv.unMute();
+        $('.fa-volume-up').hide();
+        $('.fa-volume-off').show();
+        $('.toggle-text').text('TOGGLE SOUND OFF');
+    }
+});
+
+$('.sound .next-video-btn').on('click', function () {
+    $('.sound em:nth-of-type(1)').html('~');
+    tv.pauseVideo();
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215010801) do
+ActiveRecord::Schema.define(version: 20180211080445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,18 +28,6 @@ ActiveRecord::Schema.define(version: 20180215010801) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type", using: :btree
   end
 
   create_table "comments", force: :cascade do |t|
@@ -81,11 +69,6 @@ ActiveRecord::Schema.define(version: 20180215010801) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-  end
-
-  create_table "resumes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
